f0e50852e430cd8eed54b61c3fada6a8e60358ee
Max Yevtushok <maxshok15@gmail.com>
2022-10-17 19:42:02 +0300

    Create and Index Views Update

---
+++ src/ToDoApp/Models/ToDoItem.cs
@@ -0,0 +1,21 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace ToDoApp.Models
+{
+    public class ToDoItem
+    {
+        
+        public int Id { get; set; }
+
+        [Required(ErrorMessage = "Поле Назва не може бути пустим")]
+        [MinLength(5, ErrorMessage = "Введіть мініму 5 символів")]
+        [Display(Name = "Назва задачі")]
+        public string Text { get; set; }
+
+        [Display(Name = "Статус")]
+        public bool IsComplete { get; set; }
+
+        [Display(Name = "Дата виконання")]
+        public DateTime CompleteDate { get; set; }
+    }
+}
